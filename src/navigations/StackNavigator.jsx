06e2a4d09b6108f50bd02ba6889b1b28636e5999
shahryar-rao <raoshahryar907@gmail.com>
2024-12-04 05:42:12 +0500

    1st

--- src/navigations/StackNavigator.jsx
+++ src/navigations/StackNavigator.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen } from '../screens';
+import { LoginScreen,Signup } from '../screens';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ const StackNavigator = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }} >
             <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Signup" component={Signup} />
         </Stack.Navigator>
     );
 }
